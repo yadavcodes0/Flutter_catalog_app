81d9852f2d1cdbd3d8487690ddf6babcbf2b541e
Deepak Yadav <dy22900@gmail.com>
2023-02-10 14:00:27 +0530

    day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -71,6 +71,7 @@ class _LoginPageState extends State<LoginPage> {
                       await Future.delayed(
                         const Duration(seconds: 1),
                       );
+                      // ignore: use_build_context_synchronously
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
                     child: AnimatedContainer(
