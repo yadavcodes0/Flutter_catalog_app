e1b63d4c89aee419f7b192c1b1330fb3656e30b3
Deepak Yadav <dy22900@gmail.com>
2023-02-17 15:40:32 +0530

    day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,30 @@
 import 'package:catalog_f/models/catalog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
 // ignore: must_be_immutable
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogjson = await rootBundle.loadString('assets/files/catalog.json');
+  }
 
   @override
   Widget build(BuildContext context) {
