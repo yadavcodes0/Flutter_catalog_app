f7ef80a97054e608ddb61d6cb25dd1810ced4f55
Deepak Yadav <dy22900@gmail.com>
2023-02-22 22:27:55 +0530

    day 23 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -5,12 +5,12 @@ class CatalogModel {
   static List<Item> items = [];
 
   //get item by id
-  static Item getById(int id) =>
+  Item getById(int id) =>
       // ignore: null_closures
       items.firstWhere((element) => element.id == id, orElse: null);
 
   //get item by positions
-  static Item getByPosition(int pos) => items[pos];
+  Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
